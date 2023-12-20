32c056a7b8751bd8eb97d771890d56e052e13ab4
Fabio9494 <fabio.casoria94@gmail.com>
2023-12-20 16:04:12 +0100

    Deleted variable email

--- js/script.js
+++ js/script.js
@@ -3,15 +3,13 @@ const { createApp} = Vue;
 createApp({
     data(){
         return{
-            emailList:[],
-            email:''
+            emailList:[]
         }
     },
     created(){
         for(i=0;i<10;i++){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) =>  {
-            this.email = email.data.response;  
-            this.emailList.push(this.email);
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) =>  { 
+            this.emailList.push(email.data.response);
         });
     }
     }
